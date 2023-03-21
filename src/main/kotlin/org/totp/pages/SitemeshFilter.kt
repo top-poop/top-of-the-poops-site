5029e3f2ec831952bed3633b7cae46fe36e6e82b
Top Poop <93323279+top-poop@users.noreply.github.com>
2023-03-21 08:51:59 +0000

    moving files around

---
+++ src/main/kotlin/org/totp/pages/SitemeshFilter.kt
@@ -0,0 +1,58 @@
+package org.totp.pages
+
+import org.http4k.core.Filter
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.sitemesh.SiteMeshContext
+import org.sitemesh.content.Content
+import org.sitemesh.content.ContentProcessor
+import org.sitemesh.content.tagrules.TagBasedContentProcessor
+import org.sitemesh.content.tagrules.decorate.DecoratorTagRuleBundle
+import org.sitemesh.content.tagrules.html.CoreHtmlTagRuleBundle
+import java.nio.CharBuffer
+
+typealias Http4kTransaction = Pair<Request, Response>
+
+object SitemeshFilter {
+
+    private fun contentContext(content: Content) = object : SiteMeshContext {
+        override fun getPath(): String {
+            TODO("not required")
+        }
+
+        override fun decorate(decoratorName: String?, content: Content?): Content {
+            TODO("not required")
+        }
+
+        override fun getContentToMerge(): Content {
+            return content
+        }
+
+        override fun getContentProcessor(): ContentProcessor {
+            TODO("not required")
+        }
+    }
+
+    operator fun invoke(
+        decoratorSelector: (Http4kTransaction) -> String,
+        shouldDecorate: (Http4kTransaction) -> Boolean = { true },
+        contentProcessor: ContentProcessor = TagBasedContentProcessor(
+            CoreHtmlTagRuleBundle(),
+            DecoratorTagRuleBundle()
+        ),
+    ): Filter = Filter { next ->
+        {
+            val response = next(it)
+            val tx = it to response
+            if (shouldDecorate(tx)) {
+                val content = contentProcessor.build(CharBuffer.wrap(response.bodyString()), null)
+                val decorator = contentProcessor.build(CharBuffer.wrap(decoratorSelector(tx)), contentContext(content))
+                response.body(StringBuilder().also { sb ->
+                    decorator.data.writeValueTo(sb)
+                }.toString())
+            } else {
+                response
+            }
+        }
+    }
+}
