3a6dbea0171a5027ea6056211926a54ab0f0decc
Top Poop <93323279+top-poop@users.noreply.github.com>
2024-12-30 17:44:35 +0000

    persist

---
+++ services/stream/companies.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class WaterCompany(enum.Enum):
+    Anglian = 1
+    Northumbrian = 2
+    SevernTrent = 3
+    Southern = 4
+    SouthWestWater = 5
+    ThamesWater = 6
+    UnitedUtilities = 7
+    WessexWater = 8
+    YorkshireWater = 9
