dc157380dfa85ff54913e050c0766b0737fa29dc
Top Poop <93323279+top-poop@users.noreply.github.com>
2024-12-30 13:33:41 +0000

    wip for stream stuff

--- services/stream/stream.py
+++ services/stream/stream.py
@@ -1,6 +1,8 @@
 import dataclasses
+import datetime
 import enum
-from typing import List
+import itertools
+from typing import List, Dict, Optional
 
 import requests
 from requests.adapters import HTTPAdapter
@@ -19,6 +21,34 @@ class WaterCompany(enum.Enum):
     YorkshireWater = 9
 
 
+@dataclasses.dataclass
+class FeatureRecord:
+    id: str
+    status: str
+    company: str
+    statusStart: Optional[datetime.datetime]
+    latestEventStart: Optional[datetime.datetime]
+    latestEventEnd: Optional[datetime.datetime]
+    lastUpdated: datetime.datetime
+    lat: float
+    lon: float
+    receivingWater: str
+
+
+x = {
+    "OBJECTID": 1539,
+    "Id": "NES0141",
+    "Company": "Northumbrian Water Ltd",
+    "Status": 0,
+    "StatusStart": 1735342680006,
+    "LatestEventStart": 1735342667010,
+    "LatestEventEnd": 1735342680006,
+    "Latitude": 54.60886,
+    "Longitude": -1.13235,
+    "ReceivingWaterCourse": "Tees Estuary (S Bank)",
+    "LastUpdated": 1735345436350
+}
+
 data_urls = {
     WaterCompany.Anglian: "https://services3.arcgis.com/VCOY1atHWVcDlvlJ/arcgis/rest/services/stream_service_outfall_locations_view/FeatureServer/0/query",
     WaterCompany.Northumbrian: "https://services-eu1.arcgis.com/MSNNjkZ51iVh8yBj/arcgis/rest/services/Northumbrian_Water_Storm_Overflow_Activity_2_view/FeatureServer/0/query",
@@ -36,6 +66,13 @@ data_urls = {
 class FeatureList:
     name: str
     ids: List[int]
+
+
+def timestamp(epochMs: Optional[int]) -> Optional[datetime.datetime]:
+    if epochMs is None:
+        return None
+
+    return datetime.datetime.fromtimestamp(epochMs / 1000.0, tz=datetime.UTC)
 
 
 class StreamAPI:
@@ -64,10 +101,52 @@ class StreamAPI:
             ids=resp['objectIds']
         )
 
+    def _features(self, oid: str, ids: List[int]) -> List[FeatureRecord]:
+        things = ','.join([str(id) for id in ids])
+        response = self.session.get(self.base_uri, params={
+            'where': f"1=1",
+            'outFields': '*',
+            'outSR': 4326,
+            'f': 'json',
+            'objectIds': things,
+        })
+        response.raise_for_status()
 
+        resp = response.json()
+
+        def to_record(f: Dict) -> FeatureRecord:
+            return FeatureRecord(
+                id=f["Id"],
+                status=f["Status"],
+                statusStart=timestamp(f["StatusStart"]),
+                company=f["Company"],
+                lastUpdated=timestamp(f["LastUpdated"]),
+                latestEventStart=timestamp(f["LatestEventStart"]),
+                latestEventEnd=timestamp(f["LatestEventEnd"]),
+                lat=f["Latitude"],
+                lon=f["Longitude"],
+                receivingWater=f["ReceivingWaterCourse"],
+            )
+
+        return [to_record(f["attributes"]) for f in resp["features"]]
+
+    def features(self):
+        feature_list = self._feature_list()
+
+        groups = itertools.batched(feature_list.ids, 100)
+
+        return itertools.chain.from_iterable(
+            [self._features(feature_list.name, g) for g in groups]
+        )
+
+
+import logging
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
 
-    api = StreamAPI(company=WaterCompany.Anglian)
+    api = StreamAPI(company=WaterCompany.WessexWater)
 
-    print(api._feature_list())
+    features = api.features()
+    for f in features:
+        print(f)
