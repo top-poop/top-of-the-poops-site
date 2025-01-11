bfc7f6b85136be7fd45977245bdb9aef203e2979
Top Poop <93323279+top-poop@users.noreply.github.com>
2025-01-11 22:13:52 +0000

    process stream stuff...

--- services/stream/entrypoint.sh
+++ services/stream/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 set -e
+set -x
 
-echo ">>> Stream Update <<<"
-PYTHONUNBUFFERED=1 venv/bin/python stream-download.py
+export PYTHONUNBUFFERED=1
+
+echo $(date) ">>> Processing Stream Data <<<"
+
+echo $(date) " Downloading new information from stream <<<"
+venv/bin/python stream-download.py
+
+echo $(date) " Persisting stream files to db <<<"
+venv/bin/python stream-persist-content.py
+
+echo $(date) " Processing stream files into events <<<"
+venv/bin/python stream-process-events.py
+
+echo $(date) " >>> Complete <<<"
+
