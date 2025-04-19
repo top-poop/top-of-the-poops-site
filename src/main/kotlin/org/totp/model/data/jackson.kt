bca26edf83e3c5e0ddb5f1e3c75807b74018dad6
Top Poop <93323279+top-poop@users.noreply.github.com>
2025-04-19 21:31:22 +0100

    mapping

--- src/main/kotlin/org/totp/model/data/jackson.kt
+++ src/main/kotlin/org/totp/model/data/jackson.kt
@@ -6,6 +6,7 @@ import org.http4k.format.ConfigurableJackson
 import org.http4k.format.asConfigurable
 import org.http4k.format.value
 import org.http4k.format.withStandardMappings
+import org.totp.db.StreamId
 
 
 object TotpJson : ConfigurableJackson(
@@ -14,6 +15,8 @@ object TotpJson : ConfigurableJackson(
         .withStandardMappings()
         .value(ConstituencyName)
         .value(CompanyName)
+        .value(StreamCompanyName)
+        .value(StreamId)
         .value(WaterwayName)
         .value(BathingName)
         .value(BeachName)
