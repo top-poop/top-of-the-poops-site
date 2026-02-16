c93fe65dea3b3a5310a46dd3499bd6f5fd34c854
Top Poop <93323279+top-poop@users.noreply.github.com>
2026-02-16 10:00:36 +0000

    link to company page & use EA name not stream name

--- src/main/kotlin/org/totp/pages/NowHandler.kt
+++ src/main/kotlin/org/totp/pages/NowHandler.kt
@@ -6,13 +6,36 @@ import org.http4k.template.viewModel
 import org.totp.db.StreamData
 import org.totp.http4k.pageUriFrom
 import org.totp.model.PageViewModel
+import org.totp.model.data.CompanyName
+import org.totp.model.data.RenderableCompany
+import org.totp.model.data.toRenderable
 import java.text.NumberFormat
 
 class NowPage(
     uri: Uri,
     val share: SocialShare,
-    val summary: StreamData.StreamOverflowSummary
+    val summary: RenderableStreamOverflowSummary
 ) : PageViewModel(uri)
+
+data class RenderableStreamCompanyStatus(
+    val company: RenderableCompany,
+    val count: StreamData.StreamCSOCount
+)
+
+data class RenderableStreamOverflowSummary(
+    val count: StreamData.StreamCSOCount,
+    val companies: List<RenderableStreamCompanyStatus>
+)
+
+fun StreamData.StreamOverflowSummary.toRenderable(): RenderableStreamOverflowSummary {
+    return RenderableStreamOverflowSummary(
+        count = this.count,
+        companies = this.companies.map {
+            val companyName = it.company.asCompanyName() ?: CompanyName("Unknown")
+            RenderableStreamCompanyStatus(companyName.toRenderable(), it.count)
+        }
+    )
+}
 
 object NowHandler {
     operator fun invoke(
@@ -22,7 +45,7 @@ object NowHandler {
         val viewLens = Body.viewModel(renderer, ContentType.TEXT_HTML).toLens()
 
         return { request: Request ->
-            val summary = streamData.summary()
+            val summary = streamData.summary().toRenderable()
 
             val formatted = NumberFormat.getNumberInstance().format(summary.count.start)
             Response(Status.OK)
