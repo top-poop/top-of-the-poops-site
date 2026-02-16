c0cc737c53c752f3755215af14fbe9beab3e71d9
Top Poop <93323279+top-poop@users.noreply.github.com>
2026-02-16 16:11:23 +0000

    create new grid/constituency lookup when new stream cso

--- services/stream/entrypoint.sh
+++ services/stream/entrypoint.sh
@@ -10,8 +10,14 @@ echo $(date) ">>> Processing Stream Data <<<"
 echo $(date) " Downloading new information from stream <<<"
 venv/bin/python stream-download.py
 
+echo $(date) " Downloading new information from dwr cymru <<<"
+venv/bin/python dwr-cymru-download.py
+
 echo $(date) " Persisting stream files to db <<<"
 venv/bin/python stream-persist-content.py
+
+echo $(date) " persist dwr cymru to db <<<"
+venv/bin/python stream-persist-content-dwr-cymru.py
 
 echo $(date) " Bodging CSOs that have gone away <<<"
 venv/bin/python stream-bodge-disappeared.py
@@ -19,11 +25,11 @@ venv/bin/python stream-bodge-disappeared.py
 echo $(date) " Processing stream files into events <<<"
 venv/bin/python stream-process-events.py
 
-echo $(date) " Downloading new information from dwr cymru <<<"
-venv/bin/python dwr-cymru-download.py
+echo $(date) " Find constituencies for new CSOs <<<"
+venv/bin/python grid-references-update.py
 
 echo $(date) " Processing events <<<"
-venv/bin/python stream-summarise-events.py --restart
+venv/bin/python stream-summarise-events.py
 
 echo $(date) " >>> Complete <<<"
 
---
+++ services/stream/grid-references-update.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import argparse
+import contextlib
+import logging
+import os
+import sys
+from psy import *
+import osgb
+import psycopg2
+from psycopg2.extras import DictCursor
+
+T = TypeVar('T')
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03dZ %(levelname)s [%(name)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+
+@contextlib.contextmanager
+def smart_open(filename=None):
+    if filename and filename != '-':
+        fh = open(filename, 'w')
+    else:
+        fh = sys.stdout
+
+    try:
+        yield fh
+    finally:
+        if fh is not sys.stdout:
+            fh.close()
+
+
+select_sql = """
+             select stream_id, lat, lon
+             from stream_cso sc
+                      left join stream_cso_grid sg on sc.stream_cso_id = sg.stream_cso_id
+                      left join consents_unique_view cv on sg.grid_reference = cv.effluent_grid_ref
+             where sg.grid_reference is null
+             """
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="update grid references from new locations from stream")
+
+    db_host = os.environ.get("DB_HOST", "localhost")
+
+    args = parser.parse_args()
+
+    update_materialised_views = False
+
+    with psycopg2.connect(host=db_host, database="gis", user="docker", password="docker",
+                          cursor_factory=DictCursor) as conn:
+        for cso_id, lat, lon in select_many(conn, select_sql, f=lambda row: (row["stream_id"], row["lat"], row["lon"])):
+            try:
+                result = osgb.lonlat_to_osgb(lon, lat, digits=5, formatted=False)
+                update_materialised_views = True
+                with conn.cursor() as cursor:
+                    cursor.execute(
+                        """
+    insert into grid_references(grid_reference, lat, lon, point, pcon24nm)
+        values (
+                   %(ref)s, 
+                   %(lat)s, 
+                   %(lon)s, 
+                   ST_SETSRID(ST_MakePoint(%(lon)s, %(lat)s), 4326), 
+               (select con.pcon24nm
+                   from pcon_july_2024_uk_bfc con
+                   order by ST_SETSRID(ST_MakePoint(%(lon)s, %(lat)s), 4326) <-> con.wkb_geometry limit 1))
+        on conflict (grid_reference) do nothing
+                        """,
+                        {
+                            "lat": lat,
+                            "lon": lon,
+                            "ref": result
+                        }
+                    )
+
+                logger.info(f"{cso_id}, {result}, {lat}, {lon}")
+            except osgb.gridder.FarFarAwayError:
+                logger.info(f"{cso_id}: {lon}/{lat} too far away")
+
+        conn.commit()
+
+        if update_materialised_views:
+            logger.info("Updating views...")
+            with conn.cursor() as cursor:
+                cursor.execute("refresh materialized view stream_cso_grid")
