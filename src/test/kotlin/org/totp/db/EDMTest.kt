8e5e2d35cbba8901a494c16c13d34334aaa640b9
Top Poop <93323279+top-poop@users.noreply.github.com>
2026-03-14 09:25:49 +0000

    add test

--- src/test/kotlin/org/totp/db/EDMTest.kt
+++ src/test/kotlin/org/totp/db/EDMTest.kt
@@ -2,6 +2,7 @@ package org.totp.db
 
 import org.junit.jupiter.api.Test
 import org.totp.model.data.ConstituencyName
+import org.totp.model.data.SeneddConstituencyName
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 import strikt.assertions.isGreaterThan
@@ -13,12 +14,21 @@ class EDMTest {
     val tw = EDM(testDbConnection)
 
     @Test
-    fun annualSummary() {
+    fun `annual summary for westminster constituency`() {
         val summaries = tw.annualSummariesForConstituency(ConstituencyName("Aldershot"))
 
         expectThat(summaries).size.isEqualTo(5)
         expectThat(summaries.maxOf { it.spills }).isLessThan(100)
         expectThat(summaries.maxOf { it.spills }).isGreaterThan(0)
+    }
+
+    @Test
+    fun `annual summary for senedd constituency`() {
+        val summaries = tw.annualSummariesForSeneddConstituency(SeneddConstituencyName("Ceredigion Penfro"))
+
+        expectThat(summaries).size.isEqualTo(5)
+        expectThat(summaries.maxOf { it.spills }).isLessThan(20_000)
+        expectThat(summaries.maxOf { it.spills }).isGreaterThan(5_000)
     }
 
     @Test
